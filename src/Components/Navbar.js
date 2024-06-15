aebc1de22cd5db35b3788cdb88ecfa0b271ecc25
Aditya <adityasapurba@gmail.com>
2024-06-15 09:58:03 +0530

    update github profile name

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
       <h1 className="text-2xl mt-2 font-semibold">Code Metrics</h1>
       <button className="bg-themeYellow-400 px-2 py-2 mr-4 rounded-full text-lg">
         <a
-          href="https://github.com/zorothreesword"
+          href="https://github.com/adityaapurba"
           className="mx-2 flex items-center"
         >
           <i className="ri-github-fill text-xl mr-1"></i>Github
